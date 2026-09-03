ecd3ca043fd5542985f965471ff20b02bcae30e8
IlyaSergeevich0 <defaultdev0x101@gmail.com>
2026-09-03 17:39:32 +0300

    fix: adjust log entry constructor in standard sub parsers

--- LogStandardizer.Core/Services/LogSubParser.cs
+++ LogStandardizer.Core/Services/LogSubParser.cs
@@ -19,13 +19,12 @@ public sealed record class LogSubParser(Regex LogRegex, Func<Match, LogEntry> En
             var date = DateTime.ParseExact(dateStr, "dd.MM.yyyy", CultureInfo.InvariantCulture);
             var level = LogMapper.MapLevel(levelStr);
 
-            return new LogEntry {
-                Date = date,
-                Time = timeStr,
-                Level = level,
-                Method = null,
-                Message = message
-            };
+            return new LogEntry(
+                date,
+                timeStr,
+                level,
+                null,
+                message);
         }
 
         var regex = new Regex(@"^(?<date>\d{2}\.\d{2}\.\d{4})\s+(?<time>\d{2}:\d{2}:\d{2}\.\d+)\s+(?<level>\S+)\s+(?<message>.+)$");
@@ -46,13 +45,12 @@ public sealed record class LogSubParser(Regex LogRegex, Func<Match, LogEntry> En
             var date = DateTime.ParseExact(dateStr, "yyyy-MM-dd", CultureInfo.InvariantCulture);
             var level = LogMapper.MapLevel(levelStr);
 
-            return new LogEntry {
-                Date = date,
-                Time = timeStr,
-                Level = level,
-                Method = method,
-                Message = message
-            };
+            return new LogEntry(
+                date,
+                timeStr,
+                level,
+                method,
+                message);
         }
 
         var regex = new Regex(@"^(?<date>\d{4}-\d{2}-\d{2})\s+(?<time>\d{2}:\d{2}:\d{2}\.\d+)\s*\|\s*(?<level>\S+)\s*\|\s*\d+\s*\|\s*(?<method>\S+)\s*\|\s*(?<message>.+)$");
