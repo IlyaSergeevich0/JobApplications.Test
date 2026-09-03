1a698877d76fb0a05a7a6f401f8dee17b88d8239
IlyaSergeevich0 <defaultdev0x101@gmail.com>
2026-09-03 17:34:23 +0300

    feat: implement LogStandardizer console interface

--- LogStandardizer.Console/Program.cs
+++ LogStandardizer.Console/Program.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using LogStandardizer.Core.Services;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: LogStandardizer <inputFile> <outputFile>");
+    return;
+}
+
+var inputPath = args[0];
+var outputPath = args[1];
+var problemPath = Path.Combine(Path.GetDirectoryName(outputPath) ?? ".", "problems.txt");
+
+try
+{
+    using var goodWriter = new StreamWriter(outputPath);
+    using var problemWriter = new StreamWriter(problemPath);
+
+    var logSubParsers = new List<LogSubParser>() {
+        LogSubParser.CreateStandardFormat1Parser(),
+        LogSubParser.CreateStandardFormat2Parser()
+    };
+    var logParser = new LogParser(logSubParsers);
+    var logProcessor = new LogProcessor(logParser, goodWriter, problemWriter);
+    var lines = File.ReadLines(inputPath);
+
+    logProcessor.ProcessLines(lines);
+
+    Console.WriteLine("Processing completed.");
+    Console.WriteLine($"Good records: {outputPath}");
+    Console.WriteLine($"Problem records: {problemPath}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex.Message}");
+}
